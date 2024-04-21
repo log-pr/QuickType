a565c8f322d7c5e9f6f466fc1e983cdf3493789f
Logan Price <logan.m.price@protonmail.com>
2024-04-21 15:43:36 -0400

    forgot to add semicolon in init method

--- screens/MainMenuScreen.cs
+++ screens/MainMenuScreen.cs
@@ -2,7 +2,7 @@ namespace QuickType.Screens {
 
     class MainMenuScreen : IScreen {
         static void init() {
-            Console.WriteLine("Welcome to QuickType!")
+            Console.WriteLine("Welcome to QuickType!");
         }
     }
 }
