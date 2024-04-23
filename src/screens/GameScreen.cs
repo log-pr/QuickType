939e49bb6418fb55cb99c2c5a47beccdd67e3f72
Logan Price <log.pr@pm.me>
2024-04-23 14:49:06 -0400

    add constructor to GameScreen

--- src/screens/GameScreen.cs
+++ src/screens/GameScreen.cs
@@ -2,9 +2,19 @@ namespace QuickType.Screens {
     
     class GameScreen : IScreen {
 
+        string input;
+        QuickType master;
+
+        public GameScreen(QuickType master) {
+            this.input = "";
+            this.master = master;
+        }
+
         public void init() {
             Console.Clear();
             Console.WriteLine("Starting Game...");
+
+            input = Console.ReadLine();
         }
     }
 }
